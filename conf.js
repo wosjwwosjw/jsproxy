d1ac1f7dff28aecd7324957458b3059c6da9ea12
wosjwwosjw <42485005+wosjwwosjw@users.noreply.github.com>
2020-03-30 10:35:54 +0800

    Update conf.js

--- conf.js
+++ conf.js
@@ -12,22 +12,8 @@ jsproxy_config({
 
   // 节点配置
   node_map: {
-    'demo-hk': {
-      label: '演示服务-香港节点',
-      lines: {
-        // 主机:权重
-        'node-aliyun-hk-1.etherdream.com:8443': 1,
-        'node-aliyun-hk-2.etherdream.com:8443': 2,
-      }
-    },
-    'demo-sg': {
-      label: '演示服务-新加坡节点',
-      lines: {
-        'node-aliyun-sg.etherdream.com:8443': 1,
-      },
-    },
     'mysite': {
-      label: '当前站点',
+      label: 'CF Workers反代',
       lines: {
         [location.host]: 1,
       }
@@ -42,10 +28,10 @@ jsproxy_config({
 
         // 免费版（低权重，分摊一些成本）
         // 每个账号每天 10 万次免费请求，但有频率限制
-        //'b.007.workers.dev': 1,
-        //'b.hehe.workers.dev': 1,
-        //'b.lulu.workers.dev': 1,
-        //'b.jsproxy.workers.dev': 1,
+        //'proxy.xcwosjw.workers.dev': 1,
+        //'proxy2.xcwosjw.workers.dev': 1,
+        //'proxy3.xcwosjw.workers.dev': 1,
+        //'proxy4.xcwosjw.workers.dev': 1,
       }
     }
   },
@@ -65,10 +51,10 @@ jsproxy_config({
    * 静态资源 CDN 地址
    * 用于加速 `assets` 目录中的资源访问
    */
-  // assets_cdn: 'https://cdn.jsdelivr.net/gh/zjcqoo/zjcqoo.github.io@master/assets/',
+  assets_cdn: 'https://cdn.jsdelivr.net/gh/wosjwwosjw/jsproxy@gh-pages/assets/',
 
   // 本地测试时打开，否则访问的是线上的
-  assets_cdn: 'assets/',
+  //assets_cdn: 'assets/',
 
   // 首页路径
   index_path: 'index_v3.html',
@@ -85,14 +71,5 @@ jsproxy_config({
    * URL 自定义处理（设计中）
    */
   url_handler: {
-    'https://www.baidu.com/img/baidu_resultlogo@2.png': {
-      replace: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png'
-    },
-    'https://www.pornhub.com/': {
-      redir: 'https://php.net/'
-    },
-    'http://haha.com/': {
-      content: 'Hello World'
-    },
   }
 })
